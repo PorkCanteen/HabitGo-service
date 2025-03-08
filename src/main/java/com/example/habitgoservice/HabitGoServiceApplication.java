1b6e25a36f5b0a0be3ac02c3f17129cbea55f0c2
chenmengqi <2385190394@qq.com>
2025-03-08 14:33:56 +0800

    feat: 项目初始化

---
+++ src/main/java/com/example/habitgoservice/HabitGoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.habitgoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitGoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitGoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/habitgoservice/HabitGoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habitgoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitGoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
