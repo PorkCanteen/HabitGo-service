f1405f5caa74e0cbf6434a7b406ee57a87bedbee
chenmengqi <2385190394@qq.com>
2025-04-26 12:29:05 +0800

    feat(登录): token有效期修改为7天

--- src/main/java/com/example/habitgoservice/utils/JwtUtils.java
+++ src/main/java/com/example/habitgoservice/utils/JwtUtils.java
@@ -17,7 +17,7 @@ public class JwtUtils {
     private String secret = "habitgo_jwt_secret_key";
     
     // token有效期（24小时）
-    private long expiration = 24 * 60 * 60 * 1000;
+    private long expiration = 7 *24 * 60 * 60 * 1000;
     
     // 从token中提取用户ID
     public Integer extractUserId(String token) {
