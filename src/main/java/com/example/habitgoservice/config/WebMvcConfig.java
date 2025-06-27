a10abce49ae8d351a9a7a24a36680055c3cf5aa7
chenmengqi <chenmengqi@powerlaw.ai>
2025-06-27 10:42:22 +0800

    feat(AI): deepseek问答接口开发-优化

--- src/main/java/com/example/habitgoservice/config/WebMvcConfig.java
+++ src/main/java/com/example/habitgoservice/config/WebMvcConfig.java
@@ -4,10 +4,12 @@ import com.example.habitgoservice.interceptor.JwtAuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,10 +19,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private JwtAuthInterceptor jwtAuthInterceptor;
 
     @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // 指定controller统一的接口前缀
+        configurer.addPathPrefix("/api", clazz -> clazz.isAnnotationPresent(RestController.class));
+    }
+    
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtAuthInterceptor)
                 .addPathPatterns("/api/**")  // 拦截所有的api请求
                 .excludePathPatterns("/api/user/login")  // 排除登录接口
+                .excludePathPatterns("/api/chat/test")   // 排除测试接口
                 .excludePathPatterns("/**/*.html", "/**/*.js", "/**/*.css", "/**/*.png", "/**/*.jpg")  // 排除静态资源
                 .excludePathPatterns("/**/*.ico")
                 .excludePathPatterns("/error");
